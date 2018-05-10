091835e94eaad3613687a423f876b3ffc935daa0
Liz Goff <livingearthcreations@gmail.com>
2018-05-10 14:32:44 -0500

    hooked up to database

--- server/server.js
+++ server/server.js
@@ -5,14 +5,14 @@ const PORT = process.env.PORT || 5000;
 
 
 var project_entries = require('./routes/project_entries.route');
-var project_time = require('./routes/project_time.route');
+// var project_time = require('./routes/project_time.route');
 
 
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
 
 app.use('/project_entries', project_entries);
-app.use('/project_time', project_time);
+// app.use('/project_time', project_time);
 
 
 app.listen(PORT, function() {
