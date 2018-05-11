d731bf9623a8dd9e0ec23cc532a1e4e4c01f6f2e
Liz Goff <livingearthcreations@gmail.com>
2018-05-11 12:32:48 -0500

    commit more often

--- server/public/scripts/controllers/project_time.controller.js
+++ server/public/scripts/controllers/project_time.controller.js
@@ -2,4 +2,68 @@ app.controller('TimeController', ['$http', function ($http) {
     console.log('TimeController has been loaded');
     var self = this;
     self.message = "I am the Time page!"
-}]); 
+
+    
+        self.project_time = {
+            list: []
+        };
+    
+    // add new guest
+    
+        self.enterNewGuest = function () {
+            $http({
+                method: 'GET',
+                url: '/project_time',
+            })
+                .then((response) => {
+                    console.log(response);
+                    self.project_time.list=response.data;
+                })
+                .catch((error) => {
+                    console.log('error making project_time get request', error);
+                })
+        };
+    
+    // post guest 
+    
+        self.postNewGuestToDom = function (guestToAddToDom) {
+            $http({
+                method: 'POST',
+                url: '/project_time',
+                data: guestToAddToDom
+            })
+                .then((response) => {
+                    self.project_time.guest = '';
+                    self.project_time.start_date = '';
+                    self.project_time.end_date = '';
+                    self.project_time.total_hours = '';
+                    self.project_time.image_path = '';
+                    console.log(response);
+                    self.enterNewGuest();
+                })
+                .catch((error) => {
+                    console.log('error for post on guest', error);
+                });
+        }
+    
+    // remove guest
+    
+        self.removeGuest = function (project_time) {
+            $http({
+                method: 'DELETE',
+                url: `/project_time/${project_time.id}`,
+                data: project_time
+            }).then((response) => {
+                self.enterNewGuest();
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log('error making delete request guest', error);
+                alert('Something went wrong! Check the server.');
+            });
+        }
+        
+        // Load data
+        self.enterNewGuest();
+    
+    }]); //end GuestController
--- server/routes/project_time.route.js
+++ server/routes/project_time.route.js
@@ -1,1 +1,51 @@
- 
+var express = require('express');
+var router = express.Router();
+var pool = require('../modules/time.database');
+
+router.get('/', (req, res) => {
+    console.log('GET /project_time');
+    pool.query(`SELECT * FROM "project_time";`)
+        .then((results) => {
+            res.send(results.rows);
+        })
+        .catch((error) => {
+            console.log('error with SQL GET in route guest', error);
+            res.sendStatus(500)
+        });
+});
+
+ // end route GET 
+
+router.post('/', (req, res) => {
+    console.log('POST /project_time', req.body);
+    const project_time = req.body;
+    const queryText = `INSERT INTO "project_time" ("guest", "start_date", "end_date", "total_hours", "image_path") VALUES ($1, $2, $3, $4, $5)`;
+    pool.query(queryText, [project_time.guest, project_time.start_date, project_time.end_date, project_time.total_hours, project_time.image_path])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log('error making SQL insert query in route guest', error);
+            res.sendStatus(500);
+        });
+});
+
+// end route POST
+
+router.delete('/:id', (req, res) => {
+    console.log('DELETE /project_time');
+    console.log(req.params);
+    const deleteProjectGuests = req.params.id;
+    pool.query('DELETE FROM "project_time" WHERE "id"=$1;', [deleteProjectGuests])
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('error making SQL delete in route guest', error);
+            res.sendStatus(500);
+        });
+});
+
+// end delete
+
+module.exports = router;
